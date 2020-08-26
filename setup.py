739f0fb0508323f24933393af34740336377d97a
Tom Silver <tomssilver@gmail.com>
2020-08-26 17:39:51 -0400

    setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='mit-6882-hw1',
       version='0.0.1',
-      install_requires=['git+https://github.com/tomsilver/pddlgym#egg=pddlgym'],
+      install_requires=['pddlgym @ git+https://github.com/tomsilver/pddlgym#egg=pddlgym'],
       packages=find_packages(),
       include_package_data=True,
 )
