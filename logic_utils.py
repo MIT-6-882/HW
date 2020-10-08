12c0ffe930aa4f063f1204b7eca9ed513fcf797d
Tom Silver <tomssilver@gmail.com>
2020-10-08 09:05:09 -0400

    adding more logic utils (for hw2.3)

--- logic_utils.py
+++ logic_utils.py
@@ -7,6 +7,7 @@ from .aima_utils import remove_all, unique, first, probability, isnumber, issequ
                        Expr, expr, subexpressions, extend
 from pysat.formula import CNF
 from pysat.solvers import Lingeling, Glucose3, Glucose4, Minisat22
+import itertools
 
 
 # ---------------
@@ -201,3 +202,314 @@ def is_symbol(s):
     """
     return isinstance(s, str) and s[:1].isalpha()
 
+
+# ______________________________________________________________________________
+class KB:
+    """A knowledge base to which you can tell and ask sentences.
+    To create a KB, first subclass this class and implement
+    tell, ask_generator, and retract. Why ask_generator instead of ask?
+    The book is a bit vague on what ask means --
+    For a Propositional Logic KB, ask(P & Q) returns True or False, but for an
+    FOL KB, something like ask(Brother(x, y)) might return many substitutions
+    such as {x: Cain, y: Abel}, {x: Abel, y: Cain}, {x: George, y: Jeb}, etc.
+    So ask_generator generates these one at a time, and ask either returns the
+    first one or returns False."""
+
+    def __init__(self, sentence=None):
+        if sentence:
+            self.tell(sentence)
+
+    def tell(self, sentence):
+        """Add the sentence to the KB."""
+        raise NotImplementedError
+
+    def ask(self, query):
+        """Return a substitution that makes the query true, or, failing that, return False."""
+        return first(self.ask_generator(query), default=False)
+
+    def ask_generator(self, query):
+        """Yield all the substitutions that make query true."""
+        raise NotImplementedError
+
+    def retract(self, sentence):
+        """Remove sentence from the KB."""
+        raise NotImplementedError
+
+class FolKB(KB):
+    """A knowledge base consisting of first-order definite clauses.
+    >>> kb0 = FolKB([expr('Farmer(Mac)'), expr('Rabbit(Pete)'),
+    ...              expr('(Rabbit(r) & Farmer(f)) ==> Hates(f, r)')])
+    >>> kb0.tell(expr('Rabbit(Flopsie)'))
+    >>> kb0.retract(expr('Rabbit(Pete)'))
+    >>> kb0.ask(expr('Hates(Mac, x)'))[x]
+    Flopsie
+    >>> kb0.ask(expr('Wife(Pete, x)'))
+    False
+    """
+
+    def __init__(self, clauses=None):
+        super().__init__()
+        self.clauses = []  # inefficient: no indexing
+        if clauses:
+            for clause in clauses:
+                self.tell(clause)
+
+    def tell(self, sentence):
+        if is_definite_clause(sentence):
+            self.clauses.append(sentence)
+        else:
+            raise Exception('Not a definite clause: {}'.format(sentence))
+
+    def ask_generator(self, query):
+        return fol_bc_ask(self, query)
+
+    def retract(self, sentence):
+        self.clauses.remove(sentence)
+
+    def fetch_rules_for_goal(self, goal):
+        return self.clauses
+
+
+def is_definite_clause(s):
+    """Returns True for exprs s of the form A & B & ... & C ==> D,
+    where all literals are positive. In clause form, this is
+    ~A | ~B | ... | ~C | D, where exactly one clause is positive.
+    >>> is_definite_clause(expr('Farmer(Mac)'))
+    True
+    """
+    if is_symbol(s.op):
+        return True
+    elif s.op == '==>':
+        antecedent, consequent = s.args
+        return is_symbol(consequent.op) and all(is_symbol(arg.op) for arg in conjuncts(antecedent))
+    else:
+        return False
+
+def parse_definite_clause(s):
+    """Return the antecedents and the consequent of a definite clause."""
+    assert is_definite_clause(s)
+    if is_symbol(s.op):
+        return [], s
+    else:
+        antecedent, consequent = s.args
+        return conjuncts(antecedent), consequent
+
+def standardize_variables(sentence, dic=None):
+    """Replace all the variables in sentence with new variables."""
+    if dic is None:
+        dic = {}
+    if not isinstance(sentence, Expr):
+        return sentence
+    elif is_var_symbol(sentence.op):
+        if sentence in dic:
+            return dic[sentence]
+        else:
+            v = Expr('v_{}'.format(next(standardize_variables.counter)))
+            dic[sentence] = v
+            return v
+    else:
+        return Expr(sentence.op, *[standardize_variables(a, dic) for a in sentence.args])
+standardize_variables.counter = itertools.count()
+
+def is_var_symbol(s):
+    """A logic variable symbol is an initial-lowercase string.
+    >>> is_var_symbol('EXE')
+    False
+    """
+    return is_symbol(s) and s[0].islower()
+
+def is_prop_symbol(s):
+    """A proposition logic symbol is an initial-uppercase string.
+    >>> is_prop_symbol('exe')
+    False
+    """
+    return is_symbol(s) and s[0].isupper()
+
+def variables(s):
+    """Return a set of the variables in expression s.
+    >>> variables(expr('F(x, x) & G(x, y) & H(y, z) & R(A, z, 2)')) == {x, y, z}
+    True
+    """
+    return {x for x in subexpressions(s) if is_variable(x)}
+
+
+def unify_mm(x, y, s={}):
+    """Unify expressions x,y with substitution s using an efficient rule-based
+    unification algorithm by Martelli & Montanari; return a substitution that
+    would make x,y equal, or None if x,y can not unify. x and y can be
+    variables (e.g. Expr('x')), constants, lists, or Exprs.
+    >>> unify_mm(x, 3, {})
+    {x: 3}
+    """
+
+    set_eq = extend(s, x, y)
+    s = set_eq.copy()
+    while True:
+        trans = 0
+        for x, y in set_eq.items():
+            if x == y:
+                # if x = y this mapping is deleted (rule b)
+                del s[x]
+            elif not is_variable(x) and is_variable(y):
+                # if x is not a variable and y is a variable, rewrite it as y = x in s (rule a)
+                if s.get(y, None) is None:
+                    s[y] = x
+                    del s[x]
+                else:
+                    # if a mapping already exist for variable y then apply
+                    # variable elimination (there is a chance to apply rule d)
+                    s[x] = vars_elimination(y, s)
+            elif not is_variable(x) and not is_variable(y):
+                # in which case x and y are not variables, if the two root function symbols
+                # are different, stop with failure, else apply term reduction (rule c)
+                if x.op is y.op and len(x.args) == len(y.args):
+                    term_reduction(x, y, s)
+                    del s[x]
+                else:
+                    return None
+            elif isinstance(y, Expr):
+                # in which case x is a variable and y is a function or a variable (e.g. F(z) or y),
+                # if y is a function, we must check if x occurs in y, then stop with failure, else
+                # try to apply variable elimination to y (rule d)
+                if occur_check(x, y, s):
+                    return None
+                s[x] = vars_elimination(y, s)
+                if y == s.get(x):
+                    trans += 1
+            else:
+                trans += 1
+        if trans == len(set_eq):
+            # if no transformation has been applied, stop with success
+            return s
+        set_eq = s.copy()
+
+def is_variable(x):
+    """A variable is an Expr with no args and a lowercase symbol as the op."""
+    return isinstance(x, Expr) and not x.args and x.op[0].islower()
+
+def term_reduction(x, y, s):
+    """Apply term reduction to x and y if both are functions and the two root function
+    symbols are equals (e.g. F(x1, x2, ..., xn) and F(x1', x2', ..., xn')) by returning
+    a new mapping obtained by replacing x: y with {x1: x1', x2: x2', ..., xn: xn'}
+    """
+    for i in range(len(x.args)):
+        if x.args[i] in s:
+            s[s.get(x.args[i])] = y.args[i]
+        else:
+            s[x.args[i]] = y.args[i]
+
+def occur_check(var, x, s):
+    """Return true if variable var occurs anywhere in x
+    (or in subst(s, x), if s has a binding for x)."""
+    if var == x:
+        return True
+    elif is_variable(x) and x in s:
+        return occur_check(var, s[x], s)
+    elif isinstance(x, Expr):
+        return (occur_check(var, x.op, s) or
+                occur_check(var, x.args, s))
+    elif isinstance(x, (list, tuple)):
+        return first(e for e in x if occur_check(var, e, s))
+    else:
+        return False
+
+def vars_elimination(x, s):
+    """Apply variable elimination to x: if x is a variable and occurs in s, return
+    the term mapped by x, else if x is a function recursively applies variable
+    elimination to each term of the function."""
+    if not isinstance(x, Expr):
+        return x
+    if is_variable(x):
+        return s.get(x, x)
+    return Expr(x.op, *[vars_elimination(arg, s) for arg in x.args])
+
+def subst(s, x):
+    """Substitute the substitution s into the expression x.
+    >>> subst({x: 42, y:0}, F(x) + y)
+    (F(42) + 0)
+    """
+    if isinstance(x, list):
+        return [subst(s, xi) for xi in x]
+    elif isinstance(x, tuple):
+        return tuple([subst(s, xi) for xi in x])
+    elif not isinstance(x, Expr):
+        return x
+    elif is_var_symbol(x.op):
+        return s.get(x, x)
+    else:
+        return Expr(x.op, *[subst(s, arg) for arg in x.args])
+
+def constant_symbols(x):
+    """Return the set of all constant symbols in x."""
+    if not isinstance(x, Expr):
+        return set()
+    elif is_prop_symbol(x.op) and not x.args:
+        return {x}
+    else:
+        return {symbol for arg in x.args for symbol in constant_symbols(arg)}
+
+def fol_fc_ask(kb, alpha):
+    """
+    [Figure 9.3]
+    A simple forward-chaining algorithm.
+    """
+    # TODO: improve efficiency
+    kb_consts = list({c for clause in kb.clauses for c in constant_symbols(clause)})
+
+    def enum_subst(p):
+        query_vars = list({v for clause in p for v in variables(clause)})
+        for assignment_list in itertools.product(kb_consts, repeat=len(query_vars)):
+            theta = {x: y for x, y in zip(query_vars, assignment_list)}
+            yield theta
+
+    # check if we can answer without new inferences
+    for q in kb.clauses:
+        phi = unify_mm(q, alpha)
+        if phi is not None:
+            yield phi
+
+    while True:
+        new = []
+        for rule in kb.clauses:
+            p, q = parse_definite_clause(rule)
+            for theta in enum_subst(p):
+                if set(subst(theta, p)).issubset(set(kb.clauses)):
+                    q_ = subst(theta, q)
+                    if all([unify_mm(x, q_) is None for x in kb.clauses + new]):
+                        new.append(q_)
+                        phi = unify_mm(q_, alpha)
+                        if phi is not None:
+                            yield phi
+        if not new:
+            break
+        for clause in new:
+            kb.tell(clause)
+    return None
+
+
+def fol_bc_ask(kb, query):
+    """
+    [Figure 9.6]
+    A simple backward-chaining algorithm for first-order logic.
+    KB should be an instance of FolKB, and query an atomic sentence.
+    """
+    return fol_bc_or(kb, query, {})
+
+
+def fol_bc_or(kb, goal, theta):
+    for rule in kb.fetch_rules_for_goal(goal):
+        lhs, rhs = parse_definite_clause(standardize_variables(rule))
+        for theta1 in fol_bc_and(kb, lhs, unify_mm(rhs, goal, theta)):
+            yield theta1
+
+
+def fol_bc_and(kb, goals, theta):
+    if theta is None:
+        pass
+    elif not goals:
+        yield theta
+    else:
+        first, rest = goals[0], goals[1:]
+        for theta1 in fol_bc_or(kb, subst(theta, first), theta):
+            for theta2 in fol_bc_and(kb, rest, theta1):
+                yield theta2
